f8921aa2d8f13bdfa065e3bf5bde347740cd1b62
Maria <maria.altyeva@gmail.com>
2020-04-09 11:34:16 +0800

    added long and lat

--- app/javascript/controllers/maps.js
+++ app/javascript/controllers/maps.js
@@ -1,9 +1,10 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static.targets = ["field", "map", "latitude", "longitude"]
+  static targets = ["field", "map", "latitude", "longitude"]
+
   connect() {
-    if (typeof (google) != "undefined") {
+    if (typeof (google) != "undefined"){
       this.initializeMap()
     }
   }
@@ -12,27 +13,58 @@ export default class extends Controller {
     this.map()
     this.marker()
     this.autocomplete()
+    console.log('init')
   }
 
   map() {
     if(this._map == undefined) {
       this._map = new google.maps.Map(this.mapTarget, {
         center: new google.maps.LatLng(
-          this.latitude.target.value, 
-          this.longitude.target.value),
-        zoom: 17,
+          this.latitudeTarget.value,
+          this.longitudeTarget.value
+          ),
+        zoom: 17
       })
     }
-
     return this._map
   }
 
   marker() {
+    if(this._marker == undefined) {
+    }
+    return this._marker
+
 
   }
 
   autocomplete() {
+    if(this._autocomplete == undefined) {
+      this._autocomplete = new google.maps.places.Autocomplete(this.fieldTarget);
+      this._autocomplete.bindTo("bounds", this.map())
+      this._autocomplete.setFields(['address_components', 'geometry', 'icon', 'name'])
+      this._autocomplete.addListener('place_changed', this.locationChanged.bind(this))
+    }
+    return this._autocomplete
 
+  }
+
+  locationChanged() {
+    let place = this.autocomplete().getPlace();
+
+    if (!place.geometry) {
+      // User entered the name of a Place that was not suggested and
+      // pressed the Enter key, or the Place Details request failed.
+      window.alert("No details available for input: '" + place.name + "'");
+      return;
+    }
+
+    this.map().fitBounds(place.geometry.viewport)
+    this.map().setCenter(place.geometry.location)
+    this.marker().setPosition(place.geometry.location)
+    this.marker().setVisible(true)
+
+    this.latitudeTarget.value = place.geometry.location.lat()
+    this.longitudeTarget.value = place.geometry.location.lng()
   }
 }
 
